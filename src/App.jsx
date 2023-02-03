c1c1de8682c68d55107a46d00b8856d84d1dc3a0
Mary Lueder <mlueder@gmail.com>
2023-02-03 17:59:23 -0500

    adjust profile page to only display current user

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,7 @@ const App = () => {
           path="/profile"
           element={
             <ProtectedRoute user={user}>
-              <Profile />
+              <Profile user={user}/>
             </ProtectedRoute>
           }
         />
