8fd5c7b5a98e7c59db59841220dfb3798cd9b7af
Mary Lueder <mlueder@gmail.com>
2023-02-04 14:42:32 -0500

    add rec values to reccard

--- src/components/RecCard/RecCard.jsx
+++ src/components/RecCard/RecCard.jsx
@@ -2,32 +2,31 @@ import OwnerDate from "../OwnerDate/OwnerDate";
 import { Link } from "react-router-dom";
 
 
-const RecCard = rec  => {
+const RecCard = ({rec})  => {
+  console.log('RECCARD REC ', rec)
+
   return ( 
     <>  
       <h2>Rec Card</h2>
-     
 
-    <Link to={`/recs/${rec._id}`}>
-      <article >
-            <h3>{rec.category}</h3>
-            <h3>Title: {rec.title}</h3>
-            <h3>Creator: {rec.creator}</h3>
-            <h3>Year: {rec.year}</h3>
-            <h3>Genre: {rec.genre}</h3>
-            <h3>Photo: {rec.photo}</h3>
-            <h3>Description: {rec.descripqtion}</h3>
-            <h3>Likes{rec.likes}</h3>
-            <h3>Comments{rec.comments}</h3>
-            
-
-      </article>
-    </Link>
+    {/* <Link to={`/recs/${rec._id}`}> */}
+      <div>
+        <h3>{rec.category}</h3>
+        <h3>Title: {rec.title}</h3>
+        <h3>Creator: {rec.creator}</h3>
+        {rec.year ? <h3>Year: {rec.year}</h3> : <></>}
+        {rec.genre ? <h3>Genre: {rec.genre}</h3> : <></>}
+        {rec.description ? <h3>Year: {rec.description}</h3> : <></>}
+        {rec.photo ? <img src={rec.photo} alt={rec.title} style={{width: '300px'}} /> : <></>}
+        <h3>(Likes)</h3>
+        <h3>(Comments)</h3>
+        <OwnerDate rec={rec}/>
+      </div>
+    {/* </Link> */}
 
 
-      <OwnerDate />
     </>
-)
+  )
 }
 
 export default RecCard;
