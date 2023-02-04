80c703c30a8590a6f6339cf348e4170e04b6765d
Mary Lueder <mlueder@gmail.com>
2023-02-04 14:19:28 -0500

    fix reclist

--- src/pages/RecList/RecList.jsx
+++ src/pages/RecList/RecList.jsx
@@ -2,16 +2,14 @@ import RecCard from "../../components/RecCard/RecCard";
 
 
 const RecList = ({recs}) => {
-  console.log(recs)
+  console.log('RECLIST ', recs)
 
   return ( 
     <>  
       <h1>Rec List</h1>
       {/* map over rec cards */}
+        <RecCard />   
       
-        {exampleData.map(rec =>
-            <RecCard rec={rec} />   
-        )}
     </>
   );
 }
