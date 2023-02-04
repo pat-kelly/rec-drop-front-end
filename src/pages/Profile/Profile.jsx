b957b227b65c9208568caf882d1141295c55e798
Mary Lueder <mlueder@gmail.com>
2023-02-04 12:24:57 -0500

    adjust profile component and services call to only fetch current users profile data

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,21 +1,27 @@
 import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import * as profileService from '../../services/profileService'
 
 import PlaylistList from '../../components/PlaylistList/PlaylistList'
 import { login } from '../../services/authService'
 
 const Profile = ({user}) => {
-  const [profiles, setProfiles] = useState([])
+  const {id} = useParams()
+
+  console.log(user)
+  console.log('ID ', id)
+
+  const [profile, setProfile] = useState(null)
 
   useEffect(() => {
     const fetchProfiles = async () => {
-      const profileData = await profileService.getAllProfiles()
-      setProfiles(profileData)
+      const profileData = await profileService.getProfile(id)
+      setProfile(profileData)
     }
     fetchProfiles()
-  }, [])
+  }, [id])
 
-  console.log(user.profile)
+  console.log('PROFILE ', profile)
 
   // const profile = profiles.filter(prof => (prof._id === user.profile))
   // console.log('PROFILE ', profile)
@@ -24,7 +30,10 @@ const Profile = ({user}) => {
     <>
       <h1>My Profile</h1>
       <h2>{user.name}</h2>
-      {/* <img src={profile[0].photo} alt={user.name} style={{width: '300px'}}/> */}
+      {profile
+        ? <img src={profile.photo} alt={user.name} style={{width: '300px'}}/>
+        : <h2>Loading...</h2>}
+      
       <PlaylistList/>
     </>
   )
--- src/services/profileService.js
+++ src/services/profileService.js
@@ -2,8 +2,8 @@ import * as tokenService from './tokenService'
 
 const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/profiles`
 
-async function getAllProfiles() {
-  const res = await fetch(BASE_URL, {
+async function getProfile(id) {
+  const res = await fetch(`${BASE_URL}/${id}`, {
     headers: { 'Authorization': `Bearer ${tokenService.getToken()}` },
   })
   return await res.json()
@@ -20,4 +20,4 @@ async function addPhoto(photoData, profileId) {
   return await res.json()
 }
 
-export { getAllProfiles, addPhoto }
+export { getProfile, addPhoto }
