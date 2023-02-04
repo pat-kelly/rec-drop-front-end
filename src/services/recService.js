eefa13c664c4cd08d7bb7e266ee8a80af8aaab2a
DanielleColucci <ddcolucci99@gmail.com>
2023-02-04 13:18:25 -0500

    define and export create rec service function

---
+++ src/services/recService.js
@@ -0,0 +1,23 @@
+import * as tokenService from './tokenService'
+
+const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/blogs`
+
+const create = async (recData) => {
+  try {
+    const res = fetch(BASE_URL, {
+      method: 'POST', 
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(recData)
+    })
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export {
+  create,
+}
