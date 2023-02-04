7900d1ed2e93f584dcab6ece4e31a14f7192b70f
Mary Lueder <mlueder@gmail.com>
2023-02-04 15:53:20 -0500

    add comment form

--- src/pages/RecDetails/RecDetails.jsx
+++ src/pages/RecDetails/RecDetails.jsx
@@ -8,8 +8,19 @@ import * as recService from '../../services/recService'
 
 const RecDetails = () => {
   const {id} = useParams()
-  console.log(id)
   const [rec, setRec] = useState(null)
+  const [form, setForm] = useState({
+    content: ''
+  })
+
+  const handleChange = ({ target }) => {
+    setForm({...form, [target.name]: target.value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    handleAddRec(form)
+  }
 
   useEffect(() => {
     const fetchRec = async () => {
@@ -31,6 +42,18 @@ const RecDetails = () => {
             {rec.genre ? <h3>Genre: {rec.genre}</h3> : <></>}
             {rec.description ? <h3>Description: {rec.description}</h3> : <></>}
             {rec.photo ? <img src={rec.photo} alt={rec.title} style={{width: '300px'}} /> : <></>}
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="content-input">Add A Comment:</label>
+              <input 
+                type='textarea'
+                name='content'
+                id='content-input'
+                value={form.content}
+                placeholder='Comment'
+                onChange={handleChange}
+              />
+              <button type='submit'>SUBMIT</button>
+            </form>
             <CommentList/>
             <Likes/>
           </>
