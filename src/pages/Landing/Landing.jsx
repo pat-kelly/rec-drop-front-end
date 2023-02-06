633b6f452b023646c21db9a0449609e55e7ca892
Mary Lueder <mlueder@gmail.com>
2023-02-06 10:05:21 -0500

    check changes

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -9,7 +9,7 @@ const Landing = ({ user }) => {
       <div className={styles.welcome}>
         <img src={Logo} alt="a purple narwhal" style={{width: '40vw'}}/>
         <h3 className={styles.description}>
-          Have you ever been looking for a new book, movie, or tv show, and didn’t know where to start? Rec-Drop is an app that will help you make those decisions. View a list of other people’s recommendations about their favorite shows or books, or leave one of your own! You can also make “playlists” of things you want to check out in the future.
+          Have you ever been looking for a new book, movie, song, or tv show, and didn’t know where to start? Rec-Drop will help you make those decisions. View a list of other people’s recommendations about their favorite shows or books, or leave one of your own! You can also make “playlists” of things you want to check out in the future.
         </h3>
       </div>
     </main>
