009f82d5b54049c9df021cd6aa5d0ff0e6ea9ac1
DanielleColucci <ddcolucci99@gmail.com>
2023-02-07 12:03:28 -0500

    update recs list and rec card styling

--- src/components/RecCard/RecCard.jsx
+++ src/components/RecCard/RecCard.jsx
@@ -10,37 +10,41 @@ const RecCard = ({rec, user})  => {
     <>
       {user ? 
         <Link className={styles.recCard} to={`/recs/${rec._id}`}>
-          <div className={styles.recCardHeader}>
-          <h2>{rec.title}</h2>
-          <div className={styles.iconContainer}>
-            <Icon category={rec.category}/>
+          <div className={styles.cardTop}>
+            <div className={styles.recCardHeader}>
+              <h2>{rec.title}</h2>
+              <div className={styles.iconContainer}>
+                <Icon category={rec.category}/>
+              </div>
+            </div>
+            <div className={styles.cardSubHeader}>
+              <h4>{rec.creator}</h4>
+              <h4>{rec.likes.length} ♥️</h4>
+            </div>
           </div>
-        </div>
-        <div className={styles.cardSubHeader}>
-          <h4>{rec.creator}</h4>
-          <h4>{rec.likes.length} ♥️</h4>
-        </div>
-        {rec.photo ? <img src={rec.photo} alt={rec.title} style={{maxHeight: '150px'}} /> : <></>}
-        <div className="footer">
-          <OwnerDate authorInfo={rec}/>
-        </div>
+          {rec.photo ? <img src={rec.photo} alt={rec.title} style={{maxHeight: '150px'}} /> : <></>}
+          <div className="footer">
+            <OwnerDate authorInfo={rec}/>
+          </div>
         </Link>
         :
         <div className={styles.recCard}>
-          <div className={styles.recCardHeader}>
-            <h2 className={styles.title}>{rec.title}</h2>
-            <div className={styles.iconContainer}>
-              <Icon category={rec.category}/>
+          <div className={styles.cardTop}>
+            <div className={styles.recCardHeader}>
+              <h2>{rec.title}</h2>
+              <div className={styles.iconContainer}>
+                <Icon category={rec.category}/>
+              </div>
+            </div>
+            <div className={styles.cardSubHeader}>
+              <h4>{rec.creator}</h4>
+              <h4>{rec.likes.length} ♥️</h4>
+            </div>
           </div>
-        </div>
-        <div className={styles.cardSubHeader}>
-          <h4>{rec.creator}</h4>
-          <h4>{rec.likes.length} ♥️</h4>
-        </div>
-        {rec.photo ? <img src={rec.photo} alt={rec.title} style={{maxHeight: '150px'}} /> : <></>}
-        <div className="footer">
-          <OwnerDate authorInfo={rec}/>
-        </div>
+          {rec.photo ? <img src={rec.photo} alt={rec.title} style={{maxHeight: '150px'}} /> : <></>}
+          <div className="footer">
+            <OwnerDate authorInfo={rec}/>
+          </div>
         </div>
       }
     </>
