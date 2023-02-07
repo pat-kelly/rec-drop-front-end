f6a172b3d0c47f2b6e5358223aa84c7435ee16d5
DanielleColucci <ddcolucci99@gmail.com>
2023-02-06 22:26:34 -0500

    add category icons and import in RecCard

---
+++ src/components/Icon/Icon.jsx
@@ -0,0 +1,19 @@
+import Album from '../../assets/album.png'
+import Book from '../../assets/book.png'
+import Movie from '../../assets/movie.png'
+import Song from '../../assets/song.png'
+import Tv from '../../assets/tv.png'
+
+const Icon = ({ category }) => {
+  const icons = {
+    Album: Album, 
+    Book: Book, 
+    Movie: Movie, 
+    Song: Song, 
+    "TV Show": Tv,
+  }
+
+  return <img style={{width: '30px'}} className="icon" src={icons[category]} alt={`A ${category} icon`} />
+}
+
+export default Icon
