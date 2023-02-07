faba0484886ada37398e90b2129c7bf46e4b3624
DanielleColucci <ddcolucci99@gmail.com>
2023-02-07 11:45:14 -0500

    update rec list and rec card styling

--- src/components/RecCard/RecCard.jsx
+++ src/components/RecCard/RecCard.jsx
@@ -5,9 +5,10 @@ import styles from './RecCard.module.css'
 
 const RecCard = ({rec, user})  => {
   return ( 
-    <>  
-      <div className={styles.recCard}>
-        <div className={styles.recCardHeader}>
+    <>
+      {user ? 
+        <Link className={styles.recCard} to={`/recs/${rec._id}`}>
+          <div className={styles.recCardHeader}>
           <h2>{rec.title}</h2>
           <div className={styles.iconContainer}>
             <Icon category={rec.category}/>
@@ -20,13 +21,26 @@ const RecCard = ({rec, user})  => {
         {rec.photo ? <img src={rec.photo} alt={rec.title} style={{maxHeight: '150px'}} /> : <></>}
         <div className="footer">
           <OwnerDate authorInfo={rec}/>
-          {user && 
-            <Link to={`/recs/${rec._id}`}>
-              <button>More Info</button>
-            </Link>
-            }
         </div>
-      </div>
+        </Link>
+        :
+        <div className={styles.recCard}>
+          <div className={styles.recCardHeader}>
+            <h2 className={styles.title}>{rec.title}</h2>
+            <div className={styles.iconContainer}>
+              <Icon category={rec.category}/>
+          </div>
+        </div>
+        <div className={styles.cardSubHeader}>
+          <h4>{rec.creator}</h4>
+          <h4>{rec.likes.length} ♥️</h4>
+        </div>
+        {rec.photo ? <img src={rec.photo} alt={rec.title} style={{maxHeight: '150px'}} /> : <></>}
+        <div className="footer">
+          <OwnerDate authorInfo={rec}/>
+        </div>
+        </div>
+      }
     </>
   )
 }
