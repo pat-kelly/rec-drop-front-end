654919e7388b45a5402e17607e9168be4b9ac77e
DanielleColucci <ddcolucci99@gmail.com>
2023-02-07 18:28:57 -0500

    update styling of rec details in details page

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -3,6 +3,11 @@ import Book from '../../assets/book.png'
 import Movie from '../../assets/movie.png'
 import Song from '../../assets/song.png'
 import Tv from '../../assets/tv.png'
+import Add from '../../assets/add.png'
+import Delete from '../../assets/delete.png'
+import Edit from '../../assets/edit.png'
+import Like from '../../assets/like.png'
+import Unlike from '../../assets/unlike.png'
 
 const Icon = ({ category }) => {
   const icons = {
@@ -11,6 +16,11 @@ const Icon = ({ category }) => {
     Movie: Movie, 
     Song: Song, 
     "TV Show": Tv,
+    Add: Add, 
+    Delete: Delete,
+    Edit: Edit,
+    Like: Like,
+    Unlike: Unlike,
   }
 
   return <img className="icon" src={icons[category]} alt={`A ${category} icon`} />
