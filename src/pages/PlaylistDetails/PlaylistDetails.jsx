1055421181800d878fe95a5bf8df998c917a5ede
DanielleColucci <ddcolucci99@gmail.com>
2023-02-07 13:31:03 -0500

    update playlist details styling

--- src/pages/PlaylistDetails/PlaylistDetails.jsx
+++ src/pages/PlaylistDetails/PlaylistDetails.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import RecCard from "../../components/RecCard/RecCard";
 
+import styles from './PlaylistDetails.module.css'
 import * as playlistService from '../../services/playlistService'
 
 const PlaylistDetails = ({ user, handleDeletePlaylist }) => {
@@ -9,29 +10,30 @@ const PlaylistDetails = ({ user, handleDeletePlaylist }) => {
   const [playlist, setPlaylist] = useState(null)
 
   useEffect(() => {
-    const fetchRec = async () => {
+    const fetchPlaylist = async () => {
       const data = await playlistService.show(id)
       setPlaylist(data)
     }
-    fetchRec()
+    fetchPlaylist()
   }, [id])
 
   return ( 
-    <> 
+    <main className={styles.main}> 
       {playlist 
         ? <>
-          <h1>{playlist.title}</h1>
-          <button onClick={() => handleDeletePlaylist(id)}>
-            Delete
-          </button>
-          {playlist.recs.map(rec => (
-            <RecCard key={rec._id} rec={rec} user={user}/>
-          ))}
-        </>
-        :
-        <h1>Loading...</h1>
+            <h1>{playlist.title}</h1>
+            <button onClick={() => handleDeletePlaylist(id)}>
+              Delete
+            </button>
+            <div className={styles.container}>
+              {playlist.recs.map(rec => (
+                <RecCard key={rec._id} rec={rec} user={user}/>
+              ))}
+            </div>
+          </>
+        : <h1>Loading...</h1>
       }
-    </>
+    </main>
   );
 }
 
