81b813f4e9782c14220456aa723ca3b621aa1745
DanielleColucci <ddcolucci99@gmail.com>
2023-02-08 18:55:10 -0500

    update login page styling

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -15,13 +15,15 @@ const LoginPage = props => {
   }
 
   return (
-    <main className={styles.container}>
-      <h1>Log In</h1>
-      <p>{message}</p>
-      <LoginForm
-        handleSignupOrLogin={props.handleSignupOrLogin}
-        updateMessage={updateMessage}
-      />
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <h1>Log In</h1>
+        <p>{message}</p>
+        <LoginForm
+          handleSignupOrLogin={props.handleSignupOrLogin}
+          updateMessage={updateMessage}
+        />
+      </div>
     </main>
   )
 }
