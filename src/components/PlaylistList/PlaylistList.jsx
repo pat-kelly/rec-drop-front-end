9878e2a4ce3f2a5ed8d4c782ccd426e69d213f19
DanielleColucci <ddcolucci99@gmail.com>
2023-02-08 15:34:25 -0500

    update playlist list styling:

--- src/components/PlaylistList/PlaylistList.jsx
+++ src/components/PlaylistList/PlaylistList.jsx
@@ -4,10 +4,12 @@ import styles from './PlaylistList.module.css'
 const PlaylistList = ({ playlists }) => {
   return ( 
     <>  
-      <h1 className={styles.playList}>↻  ◃◃  Play List  ▹▹   ↻</h1>
-      {playlists.map(playlist => (
-        <PlaylistCard key={playlist._id} playlist={playlist} />
-      ))}
+      <h1 className={styles.playListTitle}>↻  ◃◃  Playlists  ▹▹   ↻</h1>
+      <div className={styles.container}>
+        {playlists.map(playlist => (
+          <PlaylistCard key={playlist._id} playlist={playlist} />
+        ))}
+      </div>
     </>
   );
 }
