212a87311728f9ec4f305de1c5f20ec4286bdc83
ooh-lala <laurentozzi.code@gmail.com>
2023-02-09 10:37:03 -0500

    addphoto to owner date

--- src/components/OwnerDate/OwnerDate.jsx
+++ src/components/OwnerDate/OwnerDate.jsx
@@ -1,8 +1,10 @@
 const OwnerDate = ({authorInfo, styles}) => {
+  
   return ( 
     <div className={styles?.ownerDate}>  
       <p>{authorInfo.owner.name}</p>
       <p>{new Date(authorInfo.createdAt).toLocaleDateString()}</p>
+      <img src={authorInfo.owner.photo} alt={authorInfo.owner.name}/>
     </div>
   );
 }
