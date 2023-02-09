10ad0e9ef807729a92ff15b9a9baacd620dc6c3d
ooh-lala <laurentozzi.code@gmail.com>
2023-02-09 11:47:01 -0500

    add photo and style to owner date

--- src/components/OwnerDate/OwnerDate.jsx
+++ src/components/OwnerDate/OwnerDate.jsx
@@ -1,7 +1,12 @@
-const OwnerDate = ({authorInfo, styles}) => {
+import styles from "./OwnerDate.module.css"
+
+const OwnerDate = ({authorInfo}) => {
   return ( 
     <div className={styles?.ownerDate}>  
+    <div className={styles.ownerDiv}>
+      <img className={styles.photo} src={authorInfo.owner.photo} alt={authorInfo.owner.name} />
       <p>{authorInfo.owner.name}</p>
+      </div>
       <p>{new Date(authorInfo.createdAt).toLocaleDateString()}</p>
     </div>
   );
